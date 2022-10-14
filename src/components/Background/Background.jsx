6dea533f7a7134c98363841d56f43bcf5174a7b5
Radostin Kirilov <radostin.kirilov@elando.bg>
2022-10-14 12:17:11 +0300

    add background component

---
+++ src/components/Background/Background.jsx
@@ -0,0 +1,22 @@
+import { Card } from '@mui/material';
+
+function Background({ children }) {
+  return (
+    <Card sx={{
+      minHeight: "500px",
+      backgroundColor: "#f0efef",
+      padding: "20px",
+      display: "flex",
+      flexWrap: {
+        xs: "wrap",
+        md: "nowrap"
+      },
+      gap: "20px"
+    }}
+    >
+      {children}
+    </Card>
+  )
+}
+
+export default Background;
