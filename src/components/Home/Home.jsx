2a71f0c4d80aae309f6163dc50fb95ef9e7d87c8
Radostin Kirilov <radostin.kirilov@elando.bg>
2022-10-17 14:58:48 +0300

    Add paid leave days to home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 
 function Home() {
   const name = useSelector((state) => state.auth.name);
+  const days = useSelector((state) => state.auth.paidLeave)
 
   return (
     <>
@@ -10,7 +11,8 @@ function Home() {
         Hello {name}!
       </StyledHeading>
       <div>
-        Here in the home page will be displayed the information about how to request a time off or sick leave
+        Here in the home page will be displayed the information about how to request a time off or sick leave.
+        <p>You have <b>{days} days</b> remaining!</p>
       </div>
     </>
 
