31f65358aa156f9d4d333081143b9d3b1c4b7cfb
Radostin Kirilov <radostin.kirilov@elando.bg>
2022-10-20 10:56:41 +0300

    Extract plain text to separate file

---
+++ src/utils/messages/plainText.js
@@ -0,0 +1,14 @@
+export const plainText = {
+    sickLeaveValue: "sick-leave",
+    paidLeaveValue: "paid-leave",
+    pdf: "application/pdf",
+    png: "image/png",
+    jpeg: "image/jpeg",
+    attachPhoto: "Please attach a photo or a scanned copy of your sick leave document",
+    typeReason: "Please type in reason",
+    submit: "Submit",
+    request: "Request",
+    type: "Type",
+    paidLeave: "Paid leave",
+    sickLeave: "Sick leave",
+};
