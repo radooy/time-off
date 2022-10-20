09a176f253bc8ce1ca7abfc564f60f42e57fd630
Radostin Kirilov <radostin.kirilov@elando.bg>
2022-10-20 10:57:07 +0300

    Add additional errors to error file

--- src/utils/messages/errors.js
+++ src/utils/messages/errors.js
@@ -1,4 +1,7 @@
 export const errors = {
     email: "Please enter a valid email",
-    password: "Password must be atleast 8 symbols"
+    password: "Password must be atleast 8 symbols",
+    paidLeaveDays: "You don't have enough PTO days left to complete the request.",
+    date: "End date should be after the start date.",
+    file: "File must be less than 2 MBs and should be either pdf, png or jpeg."
 };
