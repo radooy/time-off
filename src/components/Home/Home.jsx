b7dabb506fda8a5b9fb9a55b99661a6c3a567b43
Radostin Kirilov <radostin.kirilov@elando.bg>
2022-10-26 09:29:39 +0300

    Add padding to heading and info text

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,10 +7,10 @@ function Home() {
 
   return (
     <>
-      <StyledHeading>
+      <StyledHeading className="pl-10">
         Hello {name}!
       </StyledHeading>
-      <div>
+      <div className="pl-10">
         Here in the home page will be displayed the information about how to request a time off or sick leave.
         <p>You have <b>{days} days</b> remaining!</p>
       </div>
